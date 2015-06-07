f5e148685c0a3258293761e9ff47ff41f0ed6e3f
mshmyw <mshmayoushi@gmail.com>
2015-06-07 19:44:34 +0800

    init-local

--- good-tools/.emacs.d/lisp/init-local.el
+++ good-tools/.emacs.d/lisp/init-local.el
@@ -81,4 +81,33 @@
 (global-set-key (kbd "C-,") 'tabbar-backward)
 (global-set-key (kbd "C-.") 'tabbar-forward)
 
+;;auto insert
+;;首先这句话设置一个目录，你的auto-insert 的模版文件会存放在这个目录中，
+(setq-default auto-insert-directory "~/.emacs.d/auto-insert/")
+(auto-insert-mode)  ;;; 启用auto-insert
+;; 默认情况下插入模版前会循问你要不要自动插入，这里设置为不必询问，
+;; 在新建一个org文件时，自动插入`auto-insert-directory'目录下的`org-auto-insert`文件中的内容
+(setq auto-insert-query nil) 
+(define-auto-insert "\\.org" "org-auto-insert")
+;;这个就是新建以.c 结尾的C文件时，会自动插入c-auto-insert文件中的内容
+(define-auto-insert "\\.c" "c-auto-insert")
+
+(defadvice auto-insert  (around yasnippet-expand-after-auto-insert activate)
+  "expand auto-inserted content as yasnippet templete,
+  so that we could use yasnippet in autoinsert mode"
+  (let ((is-new-file (and (not buffer-read-only)
+                          (or (eq this-command 'auto-insert)
+                              (and auto-insert (bobp) (eobp))))))
+    ad-do-it
+    (let ((old-point-max (point-max)))
+      (when is-new-file
+        (goto-char old-point-max)
+        (yas/expand-snippet (buffer-substring-no-properties (point-min) (point-max)))
+        (delete-region (point-min) old-point-max)
+        )
+      )
+    )
+  )
+
+
 (provide 'init-local) ;;must have this 
