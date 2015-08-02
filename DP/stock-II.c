b14b13a8ab0ff9f386dc3dd1924d1690b2c85ce6
mshmyw <mshmayoushi@gmail.com>
2015-08-02 21:38:59 +0800

    add comment stock II

--- DP/stock-II.c
+++ DP/stock-II.c
@@ -2,6 +2,10 @@
 //created at: Thu Jun 25 16:15:02 2015
 //author:  root
 
+/* Say you have an array for which the ith element is the price of a given stock on day i. */
+
+/* If you were only permitted to complete at most one transaction (ie, buy one and sell one share of the stock), design an algorithm to find the maximum profit. */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
