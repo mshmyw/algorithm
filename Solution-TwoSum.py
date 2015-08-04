8f42abd187db9012f8ff395f19e17386f4cce9ff
mshmyw <mshmayoushi@gmail.com>
2015-08-04 08:50:42 +0800

    solution-two sum

--- Solution-TwoSum.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def twoSum(self, num, target):
-        dictMap = {}
-        for i, v in enumerate(num):
-            if target - v in dictMap:
-                return dictMap[target - v] + 1, i + 1
-            dictMap[v] = i
-print Solution().twoSum([11, 2, 15, 7], 9)
