1985ccc4c407180754cfef601b2986425e28d66f
mshmyw <mshmayoushi@gmail.com>
2015-08-20 15:07:15 +0800

    push water

--- intel/push-water.c
+++ intel/push-water.c
@@ -3,8 +3,8 @@
 void print(int a, int b){
     printf("(%d, %d)\n", a, b);
 }
-#define BIG_CUP    4
-#define LITTLE_CUP 2
+#define BIG_CUP    8
+#define LITTLE_CUP 5
 void push_water(int big_cup_water, int little_cup_water, int target){
     if(big_cup_water == target){
         print(big_cup_water, little_cup_water);
@@ -29,14 +29,13 @@ void hanoi(char a, char b, char c, int n){ // move a to c by b
     hanoi(a, c, b, n - 1);
     move(a, c, n);
     hanoi(b, a, c, n - 1);
-
     return;
 }
 
 int main(void){
     int big_cup_water = BIG_CUP;
     int little_cup_water = LITTLE_CUP;
-    int target = 3;
+    int target = 4;
     /* push_water(big_cup_water, little_cup_water, target); */
     char a = 'a';
     char b = 'b';
