bb92bec97eb1c9d3765d7baa1bbd9d39ed4a2332
mshmyw <mshmayoushi@gmail.com>
2015-08-20 11:36:01 +0800

    push-water

--- intel/push-water.c
+++ intel/push-water.c
@@ -15,11 +15,34 @@ void push_water(int big_cup_water, int little_cup_water, int target){
     push_water(big_cup_water, little_cup_water, target);
 }
 
+void move(char a, char c, int n){
+       printf("move %d from %c to %c \n", n, a, c);
+       return;
+}
+
+void hanoi(char a, char b, char c, int n){ // move a to c by b
+    if(n == 1){
+        move(a, c, n);
+        return;
+    }
+
+    hanoi(a, c, b, n - 1);
+    move(a, c, n);
+    hanoi(b, a, c, n - 1);
+
+    return;
+}
+
 int main(void){
     int big_cup_water = BIG_CUP;
     int little_cup_water = LITTLE_CUP;
     int target = 3;
-    push_water(big_cup_water, little_cup_water, target);
-	
+    /* push_water(big_cup_water, little_cup_water, target); */
+    char a = 'a';
+    char b = 'b';
+    char c = 'c';
+    int n = 3;
+    hanoi(a, b, c, n);
+    
 	return 0;
 }
