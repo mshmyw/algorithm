c9041400a93d8d38e9088bad2091709f6458d734
摘新 <shuanghua.msh@alibaba-inc.com>
2022-04-13 20:52:21 +0800

    feat(): 汉诺塔完整版

--- typescript/hannota.ts
+++ typescript/hannota.ts
@@ -6,10 +6,23 @@
  * 3 把n-1个盘子由B 移到 C；
  */
 
+interface Inum {
+  name: string;
+  order: number;
+  data: number[];
+};
+const getRightList = (  
+  numA: Inum,
+  numB: Inum, 
+  numC: Inum, ) => {
+    const result = [numA, numB, numC].sort((a1: Inum, a2: Inum) => a1.order - a2.order);
+    return result.map((item: any) => item.data);
+};
+
 const hannuota = (
-  listA: number[],
-  listB: number[], // bridge
-  listC: number[], // target,
+  numA: Inum,
+  numB: Inum, // bridge
+  numC: Inum, // target,
   count: number,
   n: number
 ) => {
@@ -17,36 +30,47 @@ const hannuota = (
     return;
   }
   // 1 把 n-1 个盘子由 A 移到 B；
-  hannuota(listA, listC, listB, count - 1, n);
+  hannuota(numA, numC, numB, count - 1, n);
   // console.log(
   //   `执行第 ${n - count + 1}-1 步，从 A => B，移动共 ${count - 1} 个盘子 `,
-  //   listA,
-  //   listB,
-  //   listC
+  //   numA,
+  //   numB,
+  //   numC
   // );
 
   // 2 把第 n 个盘子由 A 移到 C；
-  const numN = listA.pop();
-  listC.push(numN);
+  const numN = numA.data.pop();
+  numC.data.push(numN);
   console.log(
-    `执行第 ${n - count + 1}-2 步，从 A => C，移动第 ${count} 个盘子 `,
-    listA,
-    listB,
-    listC
+    `执行第 ${n - count + 1}-2 步，从 ${numA.name} => ${
+      numC.name
+    }(A->C)，移动第 ${count} 个盘子 `,
+    getRightList(numA, numB, numC)
   );
 
   // 3 把 n-1 个盘子由 B 移到 C；
-  hannuota(listB, listA, listC, count - 1, n);
+  hannuota(numB, numA, numC, count - 1, n);
   // console.log(
   //   `执行第 ${n - count + 1}-3 步，从 B => C，移动共 ${count - 1} 个盘子 `,
-  //   listA,
-  //   listB,
-  //   listC
+  //   numA, numB, numC
   // );
-  console.log(`--end--`);  
+  // console.log(`--end--`);
 };
 
-const num1List = [3, 2, 1];
-const num2List = [];
-const num3List = [];
-hannuota(num1List, num2List, num3List, num1List.length, num1List.length);
+const numA = {
+  name: 'A',
+  order: 1,
+  data: [3, 2, 1],
+};
+const numB = {
+  name: 'B',
+  order: 2,
+  data: [],
+};
+const numC = {
+  name: 'C',
+  order: 3,
+  data: [],
+};
+const len = numA.data.length;
+hannuota(numA, numB, numC, len, len);
